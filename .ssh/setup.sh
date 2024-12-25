059d0b3521318291609636ca397178c185c80a2f
SwuduSusuwu <2002swudususuwu@gmail.com>
2024-12-24 19:04:54 -0800

    @`README.md`: linebreaks which 84429a1 forgot,
    	plus `)` which that forgot.
    	@`.ssh/setup.sh`: typo fix (for path specific to SwuduSusuwu; won' affect you).
    
    Is followup to: 84429a18161c03d62feaf1aeb78bf491f197b8c7 (+`./.ssh/` +`./README.md#Signaturecertificate`)

--- .ssh/setup.sh
+++ .ssh/setup.sh
@@ -2,7 +2,7 @@
 set -x
 echo "$(git config --get user.email) namespaces=\"git\" $(cat .ssh/id_ed25519.pub)" > .ssh/allowed_signers
 git config gpg.ssh.allowedSignersFile ".ssh/allowed_signers"
-if [ "uSwuduSusuwu" = "$(git config --get user.name)" ]; then
+if [ "SwuduSusuwu" = "$(git config --get user.name)" ]; then
 	git config gpg.format "ssh"
 	git config user.signingkey ".ssh/id_ed25519.pub"
 	git config commit.gpgsign true
