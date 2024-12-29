8e878d49349d19e184e2078e85a08b980a2c55bb
SwuduSusuwu <2002swudususuwu@gmail.com>
2024-12-28 20:29:39 -0800

    @`.ssh/setup.sh`; +`.ssh/email_of_contributor`.
    @`.ssh/setup.sh`:
    	Prefix email onto `allowed_signers` (for commands such as `git verify <ref>` or `git log --show-signature`).
    	Paths fixed (now can execute from subfolders, not just from repository root).
    
    Is followup to: commit 84429a18161c03d62feaf1aeb78bf491f197b8c7 (+`./.ssh/` +`./README.md#Signaturecertificate`).

--- .ssh/README.md
+++ .ssh/README.md
@@ -7,6 +7,7 @@
 [`./.ssh/`](./) is to [compute signatures/certificates](../README.md#signaturecertificate).
 - [`./.ssh/setup.sh`](./setup.sh) is to setup `gpg.ssh.allowedSignersFile` (allows to use `git verify <ref>` or `git log --show-signature`)
 - [`./.ssh/id_ed25519.pub`](./id_ed25519.pub) is used at `.ssh/setup.sh` to produce `.ssh/allowed_signers` + `.ssh/sha256.sig`.
+- [`./.ssh/email_of_contributor`](./email_of_contributor) is used at `.ssh/setup.sh` to produce `.ssh/allowed_signers`.
 - [`./.ssh/allowed_signers`](./allowed_signers) is required for `gpg.ssh.allowedSignersFile` (included in case `.ssh/setup.sh` fails to produce this.)
 - [`./.ssh/sha256.sig`](./sha256.sig) is for comparison to the results of `git verify <ref>` or `git log --show-signature` (included in case `.ssh/setup.sh` fails to produce this.)
 
--- .ssh/setup.sh
+++ .ssh/setup.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -x
-echo "$(git config --get user.email) namespaces=\"git\" $(cat .ssh/id_ed25519.pub)" > .ssh/allowed_signers
-git config gpg.ssh.allowedSignersFile ".ssh/allowed_signers"
-if [ "SwuduSusuwu" = "$(git config --get user.name)" ]; then
+NAME_OF_CONTRIBUTOR="SwuduSusuwu" # if you fork this, substitute your own
+DIR="$(dirname "$(git rev-parse --git-dir)")/.ssh" # `DIR=$(dirname "$0")` gives `/usr/bin` for `. setup.sh`
+if [ "${NAME_OF_CONTRIBUTOR}" = "$(git config --get user.name)" ]; then
 	git config gpg.format "ssh"
-	git config user.signingkey ".ssh/id_ed25519.pub"
+#	git config user.signingkey "${DIR}/id_ed25519.pub" # for `git commit -S`
 	git config commit.gpgsign true
+	git config --get user.email > "${DIR}/email_of_contributor"
 fi
+EMAIL_OF_CONTRIBUTOR=$(cat "${DIR}/email_of_contributor")
+echo "${EMAIL_OF_CONTRIBUTOR} namespaces=\"git\" $(cat "${DIR}/id_ed25519.pub")" > "${DIR}/allowed_signers"
+git config gpg.ssh.allowedSignersFile "${DIR}/allowed_signers" # for `git log --show-signature`, `git-verify`
 git log --show-signature
-DIR=$(dirname "$0")
-ssh-keygen -lf .ssh/id_ed25519.pub -E sha256 > "${DIR}/sha256.sig" && cat "${DIR}/sha256.sig" #GitHub converts fingerprint to sha256 hash
+ssh-keygen -lf "${DIR}/id_ed25519.pub" -E sha256 > "${DIR}/sha256.sig" && cat "${DIR}/sha256.sig" #GitHub converts fingerprint to sha256 hash
 set +x
 
