8645de5e4eb425d13f237f4141c70c80752bb5da
SwuduSusuwu <2002swudususuwu@gmail.com>
2025-02-18 05:50:50 -0800

    @`sh/make.sh`; improve paths / quotes
    	@`SUSUWU_SETUP_BINDIR()`, `SUSUWU_SETUP_OUTPUT()`, @`SUSUWU_INSTALL()`; [SC2086] quote `${OUTPUT}` path.
    	@`SUSUWU_SETUP_OBJDIR()`, @`SUSUWU_SETUP_BINDIR()`; move `${` and `}` out of the color code block. Color last instance of `OBJDIR` / `BINDIR` var.
    	@`SUSUWU_SETUP_RELEASE_DEBUG`; replace quotes (which indicate paths) with backticks (which indicate source code or commands) for `${BINDIR}${OUTPUT}`.
    	@`SUSUWU_SETUP_CXX()`, @`SUSUWU_PROCESS_INCLUDES()`, @`SUSUWU_BUILD_OBJECTS()`; comment typo fixes.
    
    @`sh/Macros.sh:SUSUWU_PRINT()`; Variable access: uses `${...}`.

--- sh/Macros.sh
+++ sh/Macros.sh
@@ -235,7 +235,7 @@ SUSUWU_PRINT() ( #/* Usage: `SUSUWU_PRINT ["<optional caller-name>"] "$(SUSUWU_S
 		elif [ "${SUSUWU_SH_HAS_FUNCNAME_RESULT}" -eq 0 ]; then
 #shellcheck disable=SC2039 #if `SUSUWU_SH_HAS_FUNCNAME`, console supports this
 			NEW_MESSAGE="${NEW_MESSAGE}${FUNCNAME[1]}(): "
-		elif [ -n "$KSH_VERSION" ]; then
+		elif [ -n "${KSH_VERSION}" ]; then
 			NEW_MESSAGE="${NEW_MESSAGE}${.sh.fun}: "
 		fi
 	fi
