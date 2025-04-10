7aa753deffb30ee247de07921b0d3e69e8a71fb7
SwuduSusuwu <2002swudususuwu@gmail.com>
2025-04-09 17:01:40 -0700

    @`.ssh/setup.sh`, @`SECURITY.md#security-policy`
    +`.ssh/allowed_signers.old`; old (not known as "compromised") certificates #TODO; warn if old certificates are used for new commits
    	@`SECURITY.md#security-policy`; document `.ssh/allowed_signers.old`
    
    @`.ssh/setup.sh`;
    	+`CRYPTO_MODE`, `:s/id_ed25519/\${CRYPTO_MODE}/`; deduplicate "magic literal", so one line allows to switch to new mode.
    	Load `${CRYPTO_MODE}.pub` from `~/.ssh/${CRYPTO_MODE.pub}`; less for forkers to do.
    	Append `.ssh/allowed_signers.old` into `.ssh/allowed_signers`.
    	Is followup to: commit 84429a18161c03d62feaf1aeb78bf491f197b8c7 (+`./.ssh/` +`./README.md#Signaturecertificate`), which introduced the original setup script and certificates.
    
    @`SECURITY.md#security-policy`;
    	Switch date format to [ISO 8601](https://wikipedia.org/wiki/ISO_8601) (national standard format).
    	Switch uppercase [Markdown fragments](https://github.github.com/gfm/#example-510) to lower.

--- .ssh/setup.sh
+++ .ssh/setup.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 set -x
 NAME_OF_CONTRIBUTOR="SwuduSusuwu" # if you fork this, substitute your own
+CRYPTO_MODE="id_ed25519"
 DIR="$(dirname "$(git rev-parse --git-dir)")/.ssh" # `DIR=$(dirname "$0")` gives `/usr/bin` for `. setup.sh`
 if [ "${NAME_OF_CONTRIBUTOR}" = "$(git config --get user.name)" ]; then
 	git config gpg.format "ssh"
-#	git config user.signingkey "${DIR}/id_ed25519.pub" # for `git commit -S`
+	cp ~/.ssh/${CRYPTO_MODE}.pub "${DIR}/${CRYPTO_MODE}.pub"
+#	git config user.signingkey "${DIR}/${CRYPTO_MODE}.pub" # for `git commit -S`
 	git config commit.gpgsign true
 	git config --get user.email > "${DIR}/email_of_contributor"
 fi
 EMAIL_OF_CONTRIBUTOR=$(cat "${DIR}/email_of_contributor")
-echo "${EMAIL_OF_CONTRIBUTOR} namespaces=\"git\" $(cat "${DIR}/id_ed25519.pub")" > "${DIR}/allowed_signers"
+echo "${EMAIL_OF_CONTRIBUTOR} namespaces=\"git\" $(cat "${DIR}/${CRYPTO_MODE}.pub")" > "${DIR}/allowed_signers"
 git config gpg.ssh.allowedSignersFile "${DIR}/allowed_signers" # for `git log --show-signature`, `git-verify`
+cat "${DIR}/allowed_signers.old" >> "${DIR}/allowed_signers" # Old certificates (not revoked) # TODO: warn if old certificates are used for new commits
 git log --show-signature
-ssh-keygen -lf "${DIR}/id_ed25519.pub" -E sha256 > "${DIR}/sha256.sig" && cat "${DIR}/sha256.sig" #GitHub converts fingerprint to sha256 hash
+ssh-keygen -lf "${DIR}/${CRYPTO_MODE}.pub" -E sha256 > "${DIR}/sha256.sig" && cat "${DIR}/sha256.sig" #GitHub converts fingerprint to sha256 hash
 set +x
 
