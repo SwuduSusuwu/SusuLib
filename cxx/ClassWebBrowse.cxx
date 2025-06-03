70fd4e68ca57ac7b3eb1d72c675586962e433d4b
SwuduSusuwu <2002swudususuwu@gmail.com>
2025-06-03 15:14:55 -0700

    @`classWebBrowseProcessUrls()`: caches `attribute`
    @`./cxx/ClassWebBrowse.cxx:classWebBrowseProcessUrls()`: `#ifdef SUSUWU_USE_PUGIXML` codeflow now caches the result of the `.attribute("href")` lookup, so CPU use improves.
    @`./posts/VirusAnalysis.md`; include this.
    TODO: test CPU use. If CPU use worsens (such as if the compiler knows best how to reuse thus `.attribute("href")` results), then `drop` this.
    Is followup to: commit c7e95f404fab7395f273e202e5042d4bc94534b2 (@`cxx/ClassWebBrowse.cxx`;`#if SUSUWU_USE_PUGIXML`), which introduces this codeflow. TODO: if improves, `squash` this.

--- cxx/ClassWebBrowse.cxx
+++ cxx/ClassWebBrowse.cxx
@@ -88,8 +88,9 @@ const std::vector<ClassIoPath> classWebBrowseProcessUrls(const ClassIoPath &loca
 	if(result) {
 #	ifdef ASSISTANTCNS_LIMIT_TO_TOP_LEVEL
 		for(pugi::xml_node node = doc.child("html").child("body").child("a"); node; node = node.next_sibling("a")) {
-			if(node.attribute("href")) {
-				urls.push_back(node.attribute("href").value());
+			const auto &attributeHref = node.attribute("href"); /* notice: if multiple execution cores access `doc`, remove `&` (do not reuse references) unless you know other execution cores just use `const` member functions */
+			if(attributeHref) {
+				urls.push_back(attributeHref.value());
 			}
 		} /* limited to direct descendants of `<body>` */
 #	else /* !def ASSISTANTCNS_LIMIT_TO_TOP_LEVEL */
