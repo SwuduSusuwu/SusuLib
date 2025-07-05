081013dd7385ea5f55434d49c427879e5878458f
SwuduSusuwu <2002swudususuwu@gmail.com>
2025-07-05 12:32:40 -0700

    +`cxx/ClassNumeral.hxx`: vector / tensor calculus
    	+`typedef long double NumeralAccum`; accumulator which can hold the sums of all non-infinite sets of Numerals. Since C++ does not include arbitrary-precision arithmetic, `long double` will have to do.
    	+`numeralAverage()`, +`numeralMagnitude`, +`numeralBalancedMagnitude`; average, standard magnitude of random-access containers (such as `std::vector`).
    	+`struct NumeralNormalizers`; stores `{average, magnitude}`.
    	+`struct NumeralNormalizersReciprocal`; stores `{average, magnitudeReciprocal}`.
    	+`numeralNormalization()`; maps training (backpropagation) inputs to `[-1, 1]`.
    	+`numeralDenormalization()`; maps inference (forwardpropagation) outputs to original values.
    	+`SUSUWU_NUMERAL_REDUNDANT`; wraps versions which (due to implicit conversions) are redundant (and can result in calls which are ambiguous).
    	@`README.md#purposes`, @`cxx/README.md#purposes`; document `cxx/ClassNumeral.hxx`.
    	@`posts/VirusAnalysis.md`; include all this.
    
    +`cxx/ClassNumeral.cxx`: placeholder for `compile_commands.json`. TODO: unit tests.
    	+`compile_commands.json`; include `cxx/ClassNumeral.cxx`.

---
+++ cxx/ClassNumeral.cxx
@@ -0,0 +1,11 @@
+/* (C) 2024 Swudu Susuwu, dual licenses: choose [GPLv2](./LICENSE_GPLv2) or [Apache 2](./LICENSE), allows all uses. */
+#ifndef INCLUDES_cxx_ClassNumeral_cxx
+#define INCLUDES_cxx_ClassNumeral_cxx
+#include "ClassNumeral.hxx" /* */
+namespace Susuwu {
+
+/* TODO: unit tests */
+
+}; /* namespace Susuwu */
+#endif /* ndef INCLUDES_cxx_ClassNumeral_cxx */
+
